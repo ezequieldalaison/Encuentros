8533552aa446528e584ef9248533ce25a5c6ef2c
Ezequiel Dalaison <DALAISON@kapsch.net>
2020-08-02 17:15:09 -0300

    ApiController to StudentController

--- Encuentros.API/Encuentros.API/Controllers/Pilates/StudentController.cs
+++ Encuentros.API/Encuentros.API/Controllers/Pilates/StudentController.cs
@@ -3,9 +3,12 @@ using Encuentros.API.Controllers.Base;
 using Encuentros.Data.Interfaces;
 using Encuentros.DTOs.Pilates;
 using Encuentros.Logic.Entities.Pilates;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Encuentros.API.Controllers.Pilates
 {
+    [Route("api/student")]
+    [ApiController]
     public class StudentController : ControllerCRUAIBase<Student, StudentDto>
     {
         public StudentController(IGenericRepository<Student> repository, IMapper mapper)
