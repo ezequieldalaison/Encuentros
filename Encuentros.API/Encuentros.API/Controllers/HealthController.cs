fe7f4ca4ee933f94b565934d1576e2216e33540d
Ezequiel Dalaison <DALAISON@kapsch.net>
2020-10-14 21:37:02 -0300

    HealthCheck

---
+++ Encuentros.API/Encuentros.API/Controllers/HealthController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Encuentros.API.Controllers
+{
+    [Route("api/health")]
+    [ApiController]
+    public class HealthController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult Get()
+        {
+            return Ok("It's alive!");
+        }
+    }
+}
